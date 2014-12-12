d936ae1ca7f95c590a69c0b9fb54d8e399114989
KM Tu <tutu522@gmail.com>
2014-12-12 22:19:42 +0900

    Changes the file names and the example codes

--- example.f90
+++ example.f90
@@ -3,7 +3,7 @@
 !  https://github.com/wesbarnett/
 !
 
-program read_xtc_prog
+program example
 
     ! 1. Use the xdr interface
     use xtc, only: xtcfile
@@ -11,35 +11,35 @@ program read_xtc_prog
     implicit none
 
     ! 2. Declare a variable of type xtcfile
-    type(xtcfile) :: xtc
+    type(xtcfile) :: xtcf
 
     ! 3. Initialize it with the name of xtc file you want to read in.
-    call xtc % init("traj.xtc")
+    call xtcf % init("example.xtc")
 
     ! 4. Read in each configuration. Everything is stored in the xtcfile type (precision, time,
     !    step, no of atoms, positions, etc.). Look in the xtc module for more details.
     !    You can save the positions in the loop for your calculations in another array, or 
     !    do your calculations after each read.
 
-    call xtc % read
+    call xtcf % read
 
-    do while ( xtc % STAT == 0 )
+    do while ( xtcf % STAT == 0 )
 
         ! Just an example to show what was read in
-        write(*,'(a,f12.6,a,i0)') " Time (ps): ", xtc % time, "  Step: ", xtc % STEP
-        write(*,'(a,f12.6,a,i0)') " Precision: ", xtc % prec, "  No. Atoms: ", xtc % NATOMS
-        write(*,'(3f9.3)') xtc % pos
+        write(*,'(a,f12.6,a,i0)') " Time (ps): ", xtcf % time, "  Step: ", xtcf % STEP
+        write(*,'(a,f12.6,a,i0)') " Precision: ", xtcf % prec, "  No. Atoms: ", xtcf % NATOMS
+        write(*,'(3f9.3)') xtcf % pos
 
         ! This is the same order as found in the GRO format fyi
-        write(*,'(11f9.5)') xtc % box(1,1), xtc % box(2,2), xtc % box(3,3), &
-                            xtc % box(1,2), xtc % box(1,3), & 
-                            xtc % box(2,1), xtc % box(2,3), &
-                            xtc % box(3,1), xtc % box(3,2) 
-        call xtc % read
+        write(*,'(11f9.5)') xtcf % box(1,1), xtcf % box(2,2), xtcf % box(3,3), &
+                            xtcf % box(1,2), xtcf % box(1,3), & 
+                            xtcf % box(2,1), xtcf % box(2,3), &
+                            xtcf % box(3,1), xtcf % box(3,2) 
+        call xtcf % read
 
     end do
 
     ! 5. Close the file
-    call xtc % close
+    call xtcf % close
 
-end program read_xtc_prog
+end program example
